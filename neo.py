c11cee37beab60f27c7e8223f534517dcbbdc4de
lausan3 <lauanthony4455@gmail.com>
2024-10-09 16:46:11 -0400

    Refactor node and edge creation methods to use the CALL { ... } IN TRANSACTIONS OF _ ROWS paradigm to fix slowdowns in long queries

--- neo.py
+++ neo.py
@@ -28,7 +28,10 @@ class Neo:
         with self.driver.session() as session:
             query = (
                 "LOAD CSV FROM 'file:///nodes.tsv' AS line FIELDTERMINATOR '\t' "
-                "MERGE (:Node{id: line[0], name: line[1], type: line[2]})"
+                "CALL { "
+                "   WITH line "
+                "   MERGE (:Node{id: line[0], name: line[1], type: line[2]}) "
+                "} IN TRANSACTIONS OF 500 ROWS "
             )
 
             result = session.run(query)
@@ -37,36 +40,49 @@ class Neo:
             time_elapsed = summary.result_available_after
 
             if nodes_created > 0:
-                print(f"{nodes_created} nodes created in Neo4j database at {self.driver.get_server_info().address} in {time_elapsed} ms")
+                print(f"{nodes_created} nodes created in Neo4j database at {self.driver.get_server_info().address}")
             else:
-                print("No nodes created in Neo4j database")
+                print("No new nodes created in Neo4j database")
+
+            print(f"Time elapsed: {time_elapsed / 1000} seconds")
 
     def _create_edges_in_graph(self):
 
         with self.driver.session() as session:
+            # Tried replacing MERGE with CREATE to test how much it affects speed.
+            # I was thinking that CREATE would be faster than MERGE because we wouldn't have to do the pattern matching on the relationship,
+            # but it ended up only saving 2 seconds when setting 10000 properties.
+            # When merging the 1,700,000 total properties, we only save about 5 minutes of runtime.
+
+            # Also, without using the CALL { ... } IN TRANSACTIONS OF ... ROWS paradigm (the new way to do USING PERIODIC COMMITS), I saw
+            # a slowdown on read times to 20 relationships created a second, which was going to take HOURS. After switching, there was no slowdown
+            # and relationships were being created live to the database as well.
             query = (
-                "LOAD CSV FROM 'file:///edges.tsv' AS line FIELDTERMINATOR '\t' "
-                "MATCH (s:Node{id:line[0]}) "
-                "MATCH (t:Node{id:line[2]}) "
-                "WITH line, substring(line[1], 1, size(line[1]) - 2) AS relationship, s, t "
-                "WITH relationship, s, t, "
-                "     CASE relationship " 
-                "        WHEN 'r' THEN 'RESEMBLES' " 
-                "        WHEN 'i' THEN 'INTERACTS' " 
-                "        WHEN 'u' THEN 'UPREGULATES' " 
-                "        WHEN 'd' THEN 'DOWNREGULATES' " 
-                "        WHEN 'e' THEN 'EXPRESSES' " 
-                "        WHEN 'a' THEN 'ASSOCIATES' " 
-                "        WHEN 'b' THEN 'BINDS' " 
-                "        WHEN 'c' THEN 'COVARIES' " 
-                "        WHEN 'l' THEN 'LOCALIZES' " 
-                "        WHEN 't' THEN 'TREATS' " 
-                "        WHEN 'p' THEN 'PALLIATES' " 
-                "        WHEN 'r>' THEN 'REGULATES' " 
-                "        ELSE null " 
-                "     END AS relationship_type "
-                "WHERE relationship_type IS NOT NULL "
-                "MERGE (s)-[r:RELATIONSHIP{code:relationship_type}]->(t) "
+                "LOAD CSV FROM 'file:///edges.tsv' AS line FIELDTERMINATOR '\t' " 
+                "CALL {  " 
+                "    WITH line " 
+                "    MATCH (s:Node{id:line[0]})  " 
+                "    MATCH (t:Node{id:line[2]})  " 
+                "    WITH line, substring(line[1], 1, size(line[1]) - 2) AS relationship, s, t  " 
+                "    WITH relationship, s, t,  " 
+                "        CASE relationship   " 
+                "            WHEN 'r' THEN 'RESEMBLES'   " 
+                "            WHEN 'i' THEN 'INTERACTS'   " 
+                "            WHEN 'u' THEN 'UPREGULATES'   " 
+                "            WHEN 'd' THEN 'DOWNREGULATES'   " 
+                "            WHEN 'e' THEN 'EXPRESSES'   " 
+                "            WHEN 'a' THEN 'ASSOCIATES'   " 
+                "            WHEN 'b' THEN 'BINDS'   " 
+                "            WHEN 'c' THEN 'COVARIES'   " 
+                "            WHEN 'l' THEN 'LOCALIZES'   " 
+                "            WHEN 't' THEN 'TREATS'   " 
+                "            WHEN 'p' THEN 'PALLIATES'   " 
+                "            WHEN 'r>' THEN 'REGULATES'   " 
+                "            ELSE null   " 
+                "        END AS relationship_type  " 
+                "    WHERE relationship_type IS NOT NULL  " 
+                "    MERGE (s)-[r:RELATES{type:relationship_type}]->(t)  " 
+                "} IN TRANSACTIONS OF 500 ROWS  "
             )
 
             result = session.run(query)
@@ -75,11 +91,22 @@ class Neo:
             time_elapsed = summary.result_available_after
 
             if relationships_created > 0:
-                print(f"{relationships_created} relationships created in Neo4j database at {self.driver.get_server_info().address} in {time_elapsed} ms")
+                print(f"{relationships_created} relationships created in Neo4j database at {self.driver.get_server_info().address}")
             else:
-                print("No relationships created in Neo4j database")
+                print("No new relationships created in Neo4j database")
 
-    def _create_nodes_in_graph_manual(self):
+            print(f"Time elapsed: {time_elapsed / 1000} seconds")
+
+    def _create_nodes_in_graph_manual_DEPRECATED(self):
+        """
+        Parses the dataset of nodes individually and creates nodes from them using the Neo4j driver.
+        This is much slower and now DEPRECATED in favor of self._create_nodes_in_graph(), which lets Neo4j handle the data parsing instead.
+        """
+
+        def create_node_query(id_raw: str, id_no_type: str, name: str, cell_type: str) -> str:
+            # The MERGE Cypher command does what the SQL CREATE IF NOT EXISTS does.
+            return (f'MERGE (n:{cell_type} {{ name: "{name}", id_raw: "{id_raw}", id: "{id_no_type}" }}) '
+                    f'RETURN n ')
 
         skipped_header = False
 
@@ -98,7 +125,7 @@ class Neo:
                     cell_type = line_items[2].rstrip('\n')
 
                     # Node creation code
-                    query = self._create_node_query(id_string, id_no_type, name, cell_type)
+                    query = create_node_query(id_string, id_no_type, name, cell_type)
                     result = session.run(query)
                     created_node = result.consume().counters.nodes_created > 0
 
@@ -107,7 +134,65 @@ class Neo:
                     else:
                         print(f"Failed to create node {id_string}. It may exist already.")
 
-    def _create_edges_in_graph_manual(self):
+    def _create_edges_in_graph_manual_DEPRECATED(self):
+        """
+        Parses the dataset of edges individually and creates relationships from them using the Neo4j driver.
+        This is much slower and now DEPRECATED in favor of self._create_edges_in_graph(), which lets Neo4j handle the data parsing instead.
+        """
+
+        def extract_relationship_from_metaedge(metaedge: str) -> (str, str, str):
+            """
+            Extract the relationship, source node's code, and target node's code as a tuple in that order.
+
+             Codes are the single capital letters representing the type of node
+             e.g. C -> Compound, G -> Gene, ect.
+
+            :param metaedge: A string in the format <SOURCE_CODE><RELATIONSHIP><TARGET_CODE> to be parsed.
+            :return: A 3-tuple of strings: (relationship, source, target).
+            """
+            source_code = metaedge[0]
+            target_code = metaedge[-1]
+            relationship = metaedge[1:-1]
+
+            match relationship:
+                case 'i':
+                    return "INTERACTS", source_code, target_code
+                case 'u':
+                    return "UPREGULATES", source_code, target_code
+                case 'e':
+                    return "EXPRESSES", source_code, target_code
+                case 'd':
+                    return "DOWNREGULATES", source_code, target_code
+                case 'a':
+                    return "ASSOCIATES", source_code, target_code
+                case 'b':
+                    return "BINDS", source_code, target_code
+                case 'c':
+                    return "COVARIES", source_code, target_code
+                case 'l':
+                    return "LOCALIZES", source_code, target_code
+                case 't':
+                    return "TREATS", source_code, target_code
+                case 'p':
+                    return "PALLIATES", source_code, target_code
+                case 'r':
+                    return "RESEMBLES", source_code, target_code
+                case 'r>':
+                    return "REGULATES", source_code, target_code
+                case _:
+                    # Tested with sys.exit(), all edges were verified as one of above.
+                    print(f"Could not parse relationship {relationship}")
+                    return relationship, source_code, target_code
+
+        def create_edge_query(source_id_raw: str, relationship: str, target_id_raw: str) -> str:
+            source_type = source_id_raw.split('::')[0]
+            target_type = target_id_raw.split('::')[0]
+
+            return (f'MATCH (s:{source_type} {{ id_raw: "{source_id_raw}" }})'
+                    f'MATCH (t:{target_type} {{ id_raw: "{target_id_raw}" }})'
+                    f'MERGE (s)-[r:{relationship}]->(t)'
+                    f'RETURN r')
+
 
         skipped_header = False
 
@@ -124,9 +209,9 @@ class Neo:
                     target = line_items[2].rstrip('\n')
                     metaedge = line_items[1]
 
-                    relationship = self._extract_relationship_from_metaedge(metaedge)
+                    relationship = extract_relationship_from_metaedge(metaedge)
 
-                    query = self._create_edge_query(source, relationship[0], target)
+                    query = create_edge_query(source, relationship[0], target)
 
                     result = session.run(query)
                     created_edge = result.consume().counters.relationships_created > 0
@@ -136,64 +221,6 @@ class Neo:
                     else:
                         print(f"Failed to create edge {source}-[{relationship[0]}]->{target}. It may exist already.")
 
-    def _extract_relationship_from_metaedge(self, metaedge: str) -> (str, str, str):
-        """
-        Extract the relationship, source node's code, and target node's code as a tuple in that order.
-
-         Codes are the single capital letters representing the type of node
-         e.g. C -> Compound, G -> Gene, ect.
-
-        :param metaedge: A string in the format <SOURCE_CODE><RELATIONSHIP><TARGET_CODE> to be parsed.
-        :return: A 3-tuple of strings: (relationship, source, target).
-        """
-        source_code = metaedge[0]
-        target_code = metaedge[-1]
-        relationship = metaedge[1:-1]
-
-        match relationship:
-            case 'i':
-                return "INTERACTS", source_code, target_code
-            case 'u':
-                return "UPREGULATES", source_code, target_code
-            case 'e':
-                return "EXPRESSES", source_code, target_code
-            case 'd':
-                return "DOWNREGULATES", source_code, target_code
-            case 'a':
-                return "ASSOCIATES", source_code, target_code
-            case 'b':
-                return "BINDS", source_code, target_code
-            case 'c':
-                return "COVARIES", source_code, target_code
-            case 'l':
-                return "LOCALIZES", source_code, target_code
-            case 't':
-                return "TREATS", source_code, target_code
-            case 'p':
-                return "PALLIATES", source_code, target_code
-            case 'r':
-                return "RESEMBLES", source_code, target_code
-            case 'r>':
-                return "REGULATES", source_code, target_code
-            case _:
-                # Tested with sys.exit(), all edges were verified as one of above.
-                print(f"Could not parse relationship {relationship}")
-                return relationship, source_code, target_code
-
-    def _create_node_query(self, id_raw: str, id_no_type: str, name: str, cell_type: str) -> str:
-        # The MERGE Cypher command does what the SQL CREATE IF NOT EXISTS does.
-        return (f'MERGE (n:{cell_type} {{ name: "{name}", id_raw: "{id_raw}", id: "{id_no_type}" }}) '
-                f'RETURN n ')
-
-    def _create_edge_query(self, source_id_raw: str, relationship: str, target_id_raw: str) -> str:
-        source_type = source_id_raw.split('::')[0]
-        target_type = target_id_raw.split('::')[0]
-
-        return (f'MATCH (s:{source_type} {{ id_raw: "{source_id_raw}" }})'
-                f'MATCH (t:{target_type} {{ id_raw: "{target_id_raw}" }})'
-                f'MERGE (s)-[r:{relationship}]->(t)'
-                f'RETURN r')
-
     def test_connect(self):
         """
         Test the connection to the remote Neo4j Aura server.
@@ -202,6 +229,16 @@ class Neo:
         self.driver.verify_connectivity()
 
         print(f"Connected to Neo4j at {URI}!")
+
+    def _reset_graph(self):
+        with self.driver.session() as session:
+            query = (
+                "MATCH (n) DETACH DELETE n"
+            )
+
+            session.run(query)
+
+            print("Reset graph complete.")
 
     def close(self):
         self.driver.close()
